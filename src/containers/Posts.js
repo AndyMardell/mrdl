3bc55b4b4389e2491ecde19237cd2e39f9dcc64a
Andy Mardell <andy@mardell.me>
2018-08-20 21:57:24 +0100

    Installs fakerpress, fetches posts and logs

--- src/containers/Posts.js
+++ src/containers/Posts.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
 
 class Posts extends Component {
+  componentDidMount () {
+    fetch('http://admin.mardell.test/wp-json/wp/v2/posts')
+      .then(response => response.json())
+      .then(response => {
+        console.log(response)
+      })
+  }
   render () {
     return (
       <div>Posts</div>
