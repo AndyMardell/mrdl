c972b722811001460407d958182ffebc48cc263a
Andy Mardell <andy@mardell.me>
2018-08-22 12:40:10 +0100

    Simple single post page

--- src/containers/PostSingle.js
+++ src/containers/PostSingle.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react'
 
 class PostSingle extends Component {
-  render () {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      post: null
+    }
+  }
+
+  componentDidMount () {
     const { slug } = this.props
 
+    fetch(process.env.REACT_APP_CMS_URL + '/wp-json/wp/v2/posts?slug=' + slug)
+      .then(response => response.json())
+      .then(response => {
+        // TODO: 404
+        if (!response.length) return false
+
+        this.setState({
+          post: response[0]
+        })
+      })
+  }
+
+  render () {
+    if (!this.state.post) return null // TODO: Loading
     return (
       <div>
-        {slug}
+        {this.state.post.title.rendered}
       </div>
     )
   }
